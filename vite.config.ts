5a96fb6ed71f21bbd4122cdba7c3c41a8eddfe10
Tanmay Singla <singlat@purdue.edu>
2025-08-14 17:11:10 -0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/Portfolio_Website/", // Required for GitHub Pages deployment
   server: {
     host: "::",
     port: 8080,
